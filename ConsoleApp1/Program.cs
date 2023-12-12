624166467901c7c2292613fa9050713158fd18af
LMN <georgelemnosanu@gmail.com>
2023-12-12 16:38:17 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class ArrayExercise1
+{
+
+    static void Main()
+    {
+
+
+        int[] array = { 199, 5, 7, 28, 4, 38, 3, 1 };
+        Array.Sort(array);
+        int dimension = array.Length;
+        Console.WriteLine("dimension:" + dimension);
+        Console.Write("Elements: ");
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i]);
+            if (i < array.Length - 1)
+            {
+                Console.Write(",");
+            }
+        }
+        Console.WriteLine();
+        Console.WriteLine("first element" + array[0]);
+        int lastElement = array.Length - 1;
+        Console.WriteLine("last element" + array[lastElement]);
+
+
+
+
+
+
+    }
+
+}
